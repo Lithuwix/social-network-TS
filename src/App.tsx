9b1f4bbfdeea515e5101d4f33f588d67f58d4b07
TokiLithuwix <loremlisumdol@gmail.com>
2022-06-30 17:06:39 +0700

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import {MainContent} from "./components/MainContent/MainContent";
 
 function App() {
   return (
     <div className="App">
      Hello, samurai! Let's go!
+        <MainContent/>
     </div>
   );
 }
---
+++ src/components/MainContent/MainContent.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+export const MainContent = () => {
+    return (
+        <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2}>
+            <Box gridColumn="span 12">
+                <div>header</div>
+            </Box>
+            <Box gridColumn="span 2">
+                <div>main-nav</div>
+            </Box>
+            <Box gridColumn="span 10">
+                <div>mainn</div>
+            </Box>
+        </Box>
+    );
+};
+
